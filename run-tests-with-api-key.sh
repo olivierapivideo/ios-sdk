17c7ebee7946c4a35b6e8d68355c22561fa34b08
Olivier Lando <olivier@api.video>
2021-07-05 15:36:23 +0200

    Add test github action

--- run-tests-with-api-key.sh
+++ run-tests-with-api-key.sh
@@ -1,5 +1,5 @@
-xcodebuild build-for-testing -scheme sdkApiVideo -workspace sdkApiVideo.xcworkspace -destination 'platform=iOS Simulator,OS=13.0,name=iPhone 11'  -derivedDataPath 'build'
+xcodebuild build-for-testing -scheme sdkApiVideo -workspace sdkApiVideo.xcworkspace -destination 'platform=iOS Simulator,OS=14.4,name=iPhone 11'  -derivedDataPath 'build'
 XCTESTRUN=`find ./build/Build/Products -name "*.xctestrun"`
-cat $XCTESTRUN | tr '\n' '\r' | sed -e "s/\(<key>EnvironmentVariables<\/key>\r[^<]*<dict>\)/\1\r\t\t\t<key>API_KEY<\/key>\r\t\t\t<string>$1<\/string>/" | tr '\r' '\n'  > tmp.xctestrun
-mv tmp.xctestrun $XCTESTRUN
-xcodebuild test-without-building -xctestrun $XCTESTRUN -destination 'platform=iOS Simulator,OS=13.0,name=iPhone 11'
+perl -0777 -i.original -pe 's/<key>OS_ACTIVITY_DT_MODE<\/key>/<key>API_KEY<\/key>\n<string>'$1'<\/string>\n<key>OS_ACTIVITY_DT_MODE<\/key>/igs' $XCTESTRUN
+cat $XCTESTRUN
+xcodebuild test-without-building -xctestrun $XCTESTRUN -destination 'platform=iOS Simulator,OS=14.4,name=iPhone 11'
